7c743f9e8ee61be84c183b20cb2845287022a7b9
CycyTech <41372520+CycyTech@users.noreply.github.com>
2018-07-31 15:54:31 +0100

    Qwikfill

---
+++ QwikFillLocationDetails_0.js
@@ -0,0 +1,15 @@
+var json_QwikFillLocationDetails_0 = {
+"type": "FeatureCollection",
+"name": "QwikFillLocationDetails_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "Area_names": "Okpoi Gonno", "B_start": "7:00am", "B_end": "4:00pm", "Nst_g_stn": "Kasapreko Gas station" }, "geometry": { "type": "Point", "coordinates": [ -0.11570646787018, 5.62869606444033 ] } },
+{ "type": "Feature", "properties": { "id": "2", "Area_names": "Baatsona", "B_start": "7:00am", "B_end": "4:00pm", "Nst_g_stn": "Kasapreko Gas station" }, "geometry": { "type": "Point", "coordinates": [ -0.08467626558506, 5.622384669323758 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Area_names": "Sakumono", "B_start": "7:00am", "B_end": "4:00pm", "Nst_g_stn": "Kasapreko Gas station" }, "geometry": { "type": "Point", "coordinates": [ -0.061573487241394, 5.623737116898806 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Area_names": "Community 18", "B_start": "7:00am", "B_end": "4:00pm", "Nst_g_stn": "Kasapreko Gas station" }, "geometry": { "type": "Point", "coordinates": [ -0.072445382932531, 5.646615544723566 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Area_names": "Community 19", "B_start": "7:00am", "B_end": "4:00pm", "Nst_g_stn": "Kasapreko Gas station" }, "geometry": { "type": "Point", "coordinates": [ -0.061120491587596, 5.652137788670536 ] } },
+{ "type": "Feature", "properties": { "id": "6", "Area_names": "Community 20", "B_start": "7:00am", "B_end": "4:00pm", "Nst_g_stn": "Kasapreko Gas station" }, "geometry": { "type": "Point", "coordinates": [ -0.07255863184598, 5.654166354853177 ] } },
+{ "type": "Feature", "properties": { "id": "7", "Area_names": "East Legon", "B_start": "7:00am", "B_end": "4:00pm", "Nst_g_stn": "Kasapreko Gas station" }, "geometry": { "type": "Point", "coordinates": [ -0.163610758259251, 5.635458197457076 ] } },
+{ "type": "Feature", "properties": { "id": "8", "Area_names": "Regimanuel Gray Estates", "B_start": "7:00am", "B_end": "4:00pm", "Nst_g_stn": "Kasapreko Gas station" }, "geometry": { "type": "Point", "coordinates": [ -0.133146800541378, 5.627005518892403 ] } }
+]
+}
